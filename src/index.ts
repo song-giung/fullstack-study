34dc6f26441c6341cb75e66653e7290eea730374
song-giung <rldnddl87@gmail.com>
2021-05-29 00:22:02 +0900

    save

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import mikroConfig from "./mikro-orm.config";
+
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import "reflect-metadata";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init(mikroConfig);
+  const orm = await MikroORM.init(mikroOrmConfig);
   orm.getMigrator().up();
 
   const app = express();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
   apolloServer.applyMiddleware({ app });
 
