e27cc28ca56be86db4a10449b4a97c5193f77dd4
song-giung <rldnddl87@gmail.com>
2021-05-28 23:54:25 +0900

    initial setup & mikroOrm & apollo-express

---
+++ README.md
@@ -0,0 +1,15 @@
+# FULLSTACK STUDY
+
+## ORIGIN RESOURCE : [Fullstack React GraphQL TypeScript Tutorial](https://youtu.be/I6ypD7qv3Z8)
+
+- React
+- TypeScript
+- GraphQL
+- URQL/Apollo
+- Node.js
+- PostgreSQL
+- MikroORM/TypeORM
+- Redis
+- Next.js
+- TypeGraphQL
+- Chakra
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
