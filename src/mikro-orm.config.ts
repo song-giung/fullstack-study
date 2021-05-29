7c28aa9cc0c722e3c0d2d4dffd07172b33f3e8ab
song-giung <rldnddl87@gmail.com>
2021-05-30 00:36:24 +0900

    add User entity

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import path from "path";
+import { User } from "./entities/Users";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lireddit",
   type: "postgresql",
   debug: !__prod__,
---
+++ src/resolvers/user.ts
@@ -0,0 +1,24 @@
+import { User } from "../entities/Users";
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => String)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const user = em.create(User, { username: options.username });
+    await em.persistAndFlush(user);
+    return "bye";
+  }
+}
